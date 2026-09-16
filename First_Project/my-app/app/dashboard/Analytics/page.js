0554c6895a805d163d6390006b7df435fc6a894e
Minhaz Reza <minhazreza3243@gmail.com>
2026-09-17 05:20:39 +0600

    Routes , Pages , Layout , Template & metadata practice

---
+++ First_Project/my-app/app/dashboard/Analytics/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Analytics Page",
+  description: "View your application analytics here.",
+};
+
+export default function AnalyticsPage() {
+  return (
+    <div>
+      <h1 className="m-2 font-extrabold text-6xl">Analytics Page</h1>
+      <p className="m-2 text-2xl">View your application analytics here.</p>
+    </div>
+  );
+}
---
+++ First_Project/my-app/app/dashboard/settings/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Settings Page",
+  description: "Manage your application settings here.",
+};
+
+export default function SettingsPage() {
+  return (
+    <div>
+      <h1 className="m-2 font-extrabold text-6xl">Settings Page</h1>
+      <p className="m-2 text-2xl">Manage your application settings here.</p>
+    </div>
+  );
+}
